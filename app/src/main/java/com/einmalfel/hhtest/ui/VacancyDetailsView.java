98a5bd6d31e6bc2024e8e7bbfe76b43fdbd3f9ef
Vasily Makarov <einmalfel@gmail.com>
2017-12-05 01:44:00 +0300

    add VacancyDetailsView interface

---
+++ app/src/main/java/com/einmalfel/hhtest/ui/VacancyDetailsView.java
@@ -0,0 +1,9 @@
+package com.einmalfel.hhtest.ui;
+
+import android.support.annotation.Nullable;
+
+import com.einmalfel.hhtest.data.Vacancy;
+
+public interface VacancyDetailsView {
+  void setData(@Nullable Vacancy vacancy);
+}
